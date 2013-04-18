d2a98e0701815701c735be3d26235ce81053085b
irene Janzen <corsa1234@gmx.de>
2013-04-18 09:55:06 +0200

    Einfach nur ein tes

---
+++ Phase2/HelloKitty.java
@@ -0,0 +1,11 @@
+public class HelloKitty {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Kitty");
+	}
+
+}
